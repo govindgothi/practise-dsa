11f43ddae67988324f3ef44a70753e990494b508
Govind <govindgothi8838@gmail.com>
2025-10-09 09:46:30 +0530

    change in condition for accuracy

--- Warm-up/12_printRigthAngledStarTriangle.cpp
+++ Warm-up/12_printRigthAngledStarTriangle.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int row = 4;
     for(int i = 0; i<row; i++){
-        for(int j = 0; j< i+1;j++){
+        for(int j = 0; j<= i;j++){
             cout<<"* ";
         }
         cout<<endl;
