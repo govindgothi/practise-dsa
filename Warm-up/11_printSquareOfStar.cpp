69fe46543397e0c72c8a4af5451f6701bb9e10b3
Govind <govindgothi8838@gmail.com>
2025-10-09 09:39:33 +0530

    print squart of start

---
+++ Warm-up/11_printSquareOfStar.cpp
@@ -0,0 +1,13 @@
+// print square of start, size 4 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int size = 4;
+    for(int i=0;i<size;i++ ){
+        for(int j=0;j<size;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
