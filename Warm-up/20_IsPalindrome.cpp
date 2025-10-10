25e653cba999a0afe4443f42bdbdc1006ddfcb31
Govind <govindgothi8838@gmail.com>
2025-10-10 23:41:11 +0530

    add palindrome question

--- Warm-up/20_IsPalindrome.cpp
+++ Warm-up/20_IsPalindrome.cpp
@@ -3,31 +3,31 @@
 #include<iostream>
 using namespace std;
 
-bool isPalidrone(int val){
-  if(val == 0) return true;
- int count = 0;
-  while(val>0){
-    count++;
-    val = val/10;
-  }
- if(count%2==0){
-    return false;
- };
- for(int i = 0; i < count/2;i++){
-    if(val){
-       
-    }
- }
-
-
+bool checkPalindrome(int val){
+   if(val == 0) return true;
+   if(val < 0) return false;
+   int copyVal = val;
+   int reverseVal = 0;
+   while(val>0){
+     int rem = val%10;
+     reverseVal = reverseVal*10 + rem;
+     val = val/10;
+   }
+   if(reverseVal == copyVal){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 int main(){
- int val = 43234;
- bool isPali = isPalidrone(val);
- if(isPali == 1){
-    cout<<"This number is palindrome";
- }else{
-    cout<<"This number is not palindrome";
- }
+   int val = 1233421;
+   bool isPalindrome = checkPalindrome(val);
+   if(isPalindrome == 1){
+      cout<<"The value is PALINDROME";
+      return 1;
+   }else{
+      cout<<"The value is NOT PALINDROME";
+      return 0;
+   }
 }
